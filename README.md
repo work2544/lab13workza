ba2db872a35fb4802e80cf50ee361e0323f26f83
work2544 <work21_123@hotmail.com>
2021-01-22 16:45:09 +0700

    complete

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME: ลัทธพล เลาหพิลูลรัตนา
+# STUDENT ID: 630610759
--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+    for (int i = 1; i < N; i++)
+	{
+		    T temp=d[i];
+			for (int j = i; j >0 ; j--)
+			{
+				if(temp>d[j-1])
+				{
+					d[j]=d[j-1];
+					d[j-1]=temp;
+					
+				}
+				else
+				{
+					break;
+				}
+				
+			}
+			cout << "Pass "<<i<<":";
+	for(int i = 0; i < 10; i++) cout << d[i] << " ";
+	cout<<endl;
+	}
+	
 
+	
 }
 
 int main(){
